56619951e808951d4e581f971bbeb6024901b96c
Ignacio Toledo <itoledoc@gmail.com>
2014-05-27 10:09:51 -0400

    Works on readProjects

--- readProjects.py
+++ readProjects.py
@@ -17,8 +17,9 @@ class DataBase(object):
             self.projects, self.not_ready_prj = get_projects()
         else:
             self.projects, self.not_ready_prj = get_projects(source)
-        self.projects['obsproj'] = pd.Series(np.zeros(len(self.projects), dtype=object),
-                                             index=self.projects.index)
+        self.projects['obsproj'] = pd.Series(
+            np.zeros(len(self.projects), dtype=object),
+            index=self.projects.index)
         for p in self.projects.index:
             obspro = ObsProject(path + p + '.xml')
             self.projects.loc[p, 'obsproj'] = obspro
@@ -46,7 +47,8 @@ class ObsProject(object):
             # science_goal_uid = sg.attrib['entityPartId']
             for ous in sg.ObsUnitSet:
                 for mous in ous.ObsUnitSet:
-                    array_requested = mous.ObsUnitControl.attrib['arrayRequested']
+                    array_requested = mous.ObsUnitControl.attrib[
+                        'arrayRequested']
                     try:
                         for sbs in mous.SchedBlockRef:
                             if array_requested == 'TWELVE-M':
@@ -55,7 +57,7 @@ class ObsProject(object):
                                 sched_uid_7m.append(sbs.attrib['entityId'])
                             elif array_requested == 'TP-Array':
                                 sched_uid_tp.append(sbs.attrib['entityId'])
-                    except AttributeError, e:
+                    except AttributeError:
                         # Member OUS does not have any SB created yet.
                         continue
         return sched_uid_7m, sched_uid_12m, sched_uid_tp
@@ -86,21 +88,30 @@ def get_projects(source=None, path='./'):
 
     connection = cx_Oracle.connect(conx_strin)
     cursor = connection.cursor()
-    sql1 = "SELECT obs1.PRJ_CODE, obs1.PRJ_ARCHIVE_UID, obs1.PRJ_SCIENTIFIC_RANK, " \
-           "obs1.PRJ_LETTER_GRADE, obs2.DOMAIN_ENTITY_STATE, obs1.PRJ_VERSION, obs1.PRJ_TIME_OF_CREATION " \
+    sql1 = "SELECT PRJ_ARCHIVE_UID,DELETED,PI,PRJ_NAME,ARRAY,PRJ_CODE, " \
+           "CODE,PRJ_TIME_OF_CREATION,PRJ_SCIENTIFIC_RANK,PRJ_VERSION," \
+           "PRJ_ASSIGNED_PRIORITY,PRJ_LETTER_GRADE,DOMAIN_ENTITY_STATE," \
+           "OBS_PROJECT_ID,PROJECT_WAS_TIMED_OUT " \
            "FROM ALMA.BMMV_OBSPROJECT obs1, ALMA.OBS_PROJECT_STATUS obs2  " \
-           "WHERE (PRJ_CODE LIKE '2013%A' OR PRJ_CODE LIKE '2013%S' OR PRJ_CODE LIKE '2013%T') " \
-           "AND (PRJ_LETTER_GRADE='A' OR PRJ_LETTER_GRADE='B' OR PRJ_LETTER_GRADE='C') " \
+           "WHERE (PRJ_CODE LIKE '2013%A' OR PRJ_CODE LIKE '2013%S' OR " \
+           "PRJ_CODE LIKE '2013%T') " \
+           "AND (PRJ_LETTER_GRADE='A' OR PRJ_LETTER_GRADE='B' " \
+           "OR PRJ_LETTER_GRADE='C') " \
            "AND OBS2.OBS_PROJECT_ID = OBS1.PRJ_ARCHIVE_UID"
-    ltype = [('prj_code', 'a30'), ('prj_uid', 'a30'), ('rank', 'i4'), ('grade', 'a4'), ('status', 'a20'),
-             ('version', 'a6'), ('timestamp', 'datetime64[us]')]
+
+    states = ["Approved", "Phase1Submitted", "Broken", "Completed", "Canceled"]
 
     if source is None:
         cursor.execute(sql1)
-        temp_projects = np.array(cursor.fetchall(), dtype=ltype)
+        df1 = pd.DataFrame(cursor.fetchall(),
+                           columns=[rec[0] for rec in cursor.description])
+        projects = df1.query(
+            'DOMAIN_ENTITY_STATE not in states').set_index('PRJ_CODE')
+        not_ready_projects = df1.query(
+                'DOMAIN_ENTITY_STATE in states').set_index('PRJ_CODE')
     else:
         if type(source) is not str and type(source) is not list:
-            print "The filename shoud be a string or a list"
+            print "The filename should be a string or a list"
             return None
         try:
             if type(source) is str:
@@ -108,42 +119,47 @@ def get_projects(source=None, path='./'):
                 read_csv = csv.reader(fp)
             else:
                 read_csv = source
-            projects_tuple = []
+            c = 0
             for l in read_csv:
                 if type(source) is str:
                     l = l[0]
                 sql2 = sql1 + ' AND OBS1.PRJ_CODE = ' + '\'%s\'' % l
                 cursor.execute(sql2)
-                projects_tuple.append(cursor.fetchall()[0])
-            temp_projects = np.array(projects_tuple, dtype=ltype)
+                if c == 0:
+                    df2 = pd.DataFrame(
+                        cursor.fetchall(),
+                        columns=[rec[0] for rec in cursor.description])
+                else:
+                    df2.ix[c] = pd.Series(cursor.fetchall()[0], index=df2
+                                          .columns)
+                c += 1
+            projects = df2.query(
+                'DOMAIN_ENTITY_STATE not in states').set_index('PRJ_CODE')
+            not_ready_projects = df2.query(
+                'DOMAIN_ENTITY_STATE in states').set_index('PRJ_CODE')
         except IOError:
             print "Filename does not exist"
             return None
+    obs_proj_id = projects.OBS_PROJECT_ID
+    timestamp = pd.Series(np.zeros(len(obs_proj_id), dtype=object),
+                          index=obs_proj_id.index)
 
-    projects = np.zeros(0, dtype=ltype)
-    not_ready_prj = np.zeros(0, dtype=ltype)
-    for p in temp_projects:
-        if p['status'] in ['Approved', 'Phase1Submitted', 'Broken', 'Completed', 'Canceled']:
-            # print "Project %s is in %s state. ObsProject.xml not downloaded" % (p['prj_code'], p['status'])
-            not_ready_prj = np.concatenate((not_ready_prj, np.array([p], dtype=ltype)))
-            continue
-
+    for i1 in obs_proj_id.index:
         cursor.execute("SELECT TIMESTAMP, XMLTYPE.getClobVal(xml) "
                        "from ALMA.xml_obsproject_entities "
-                       "where archive_uid = '%s'" % p['prj_uid'])
+                       "where archive_uid = '%s'" % obs_proj_id.ix[i1])
         data = cursor.fetchall()[0]
         xml_content = data[1].read()
-        p['timestamp'] = data[0]
-        projects = np.concatenate((projects, np.array([p], dtype=ltype)))
-        filename = path + p['prj_code'] + '.xml'
+        timestamp.ix[i1] = data[0]
+        filename = path + i1 + '.xml'
         io_file = open(filename, 'w')
         io_file.write(xml_content)
         io_file.close()
 
+    projects['timestamp'] = timestamp
     cursor.close()
     connection.close()
-    return pd.DataFrame.from_records(projects, index='prj_code'), pd.DataFrame.from_records(not_ready_prj,
-                                                                                            index='prj_code')
+    return projects, not_ready_projects
 
 
 def get_schedblocks(uid_list, path='./'):
