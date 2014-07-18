33f97ad024fe34b1fa52d5799ee9617e237f7138
Ignacio Toledo <itoledoc@gmail.com>
2014-07-18 13:16:56 -0400

    working

--- gwto2BL.py
+++ gwto2BL.py
@@ -602,7 +602,7 @@ class MyStdTableModel(QAbstractTableModel):
                 return QVariant(QString("%1").arg(sb[col], 0, 'f', 2))
             elif col in [9, 19, 20, 25, 26]:
                 return QVariant(QString("%1").arg(sb[col], 0, 'f', 1))
-            elif col in [13, 14, 15]:
+            elif col in [14, 15]:
                 return QVariant(QString("%1").arg(sb[col], 0, 'i', 0))
 
             return QVariant(str(self.arraydata[index.row()][index.column()]))
