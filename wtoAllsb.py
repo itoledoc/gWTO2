05336c21c2d2b6f985e36b64e98368b15a9b64e8
Ignacio Toledo <itoledoc@gmail.com>
2014-08-29 09:37:37 -0400

    working

--- wtoAllsb.py
+++ wtoAllsb.py
@@ -1,46 +1,491 @@
 __author__ = 'itoledo'
+__metaclass__ = type
 
-from datetime import datetime
-from datetime import timedelta
-
+import numpy as np
 import pandas as pd
-import ephem
+import csv
+import cx_Oracle
+import os
 from lxml import objectify
+from subprocess import call
+import arrayResolution2p as ARes
 
-from wtoDatabase import WtoDatabase
-import ruvTest as rUV
+conx_string = 'almasu/alma4dba@ALMA_ONLINE.OSF.CL'
+conx_string_sco = 'almasu/alma4dba@ALMA_ONLINE.SCO.CL'
+prj = '{Alma/ObsPrep/ObsProject}'
+val = '{Alma/ValueTypes}'
+sbl = '{Alma/ObsPrep/SchedBlock}'
 
 
 pd.options.display.width = 200
 pd.options.display.max_columns = 55
 
-SSO = ['Moon', 'Sun', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn',
-       'Uranus', 'Neptune', 'Pluto']
-MOON = ['Ganymede', 'Europa', 'Callisto', 'Io', 'Titan']
 
-alma1 = ephem.Observer()
-alma1.lat = '-23.0262015'
-alma1.long = '-67.7551257'
-alma1.elev = 5060
+# noinspection PyPep8Naming
+class WtoDatabase(object):
 
-h = 6.6260755e-27
-k = 1.380658e-16
-c = 2.99792458e10
-c_mks = 2.99792458e8
-
-
-class WtoAllsb(WtoDatabase):
     """
-    Inherits from WtoDatabase, adds the methods for selection and scoring.
-    It also sets the default parameters for these methods: pwv=1.2, date=now,
-    array angular resolution, transmission=0.5, minha=-5, maxha=3, etc.
+    WtoDatabase is the class that stores the Projects and SB information in
+    dataframes, and it also has the methods to connect and query the OSF
+    archive for this info.
 
-    :param path: A path, relative to $HOME, where the cache is stored.
-    :type path: (default='/.wto/') String.
-    :param source: See WtoDatabase definitions.
-    :param forcenew: See WtoDatabase definitions.
-    :return: A WtoAlgorithm instance.
+    A default instance will use the directory $HOME/.wto as a cache, and by
+    default find the approved Cycle 2 projects and carried-over Cycle 1
+    projects. If a file name or list are given as 'source' parameter, only the
+    information of the projects in that list or filename will be ingested.
+
+    Setting *forcenew* to True will force the cleaning of the cache dir, and
+    all information will be processed again.
+
+    :param path: Path for data cache.
+    :type path: str, default '$HOME/.wto'
+    :param source: File or list of strings with the codes of the projects
+        to be ingested by WtoDatabase.
+    :type source: list or str
+    :param forcenew: Force cache cleaning and reload from archive.
+    :type forcenew: boolean, default False
     """
-    def __init__(self, path='/.wto/', source=None, forcenew=False):
 
-        super(WtoAllsb, self).__init__(path, source, forcenew)
+    def __init__(self, path='/.wto_all/', source=None, forcenew=False):
+        """
+
+
+        """
+        self.source = source
+        self.new = forcenew
+        # Default Paths and Preferences
+        if path[-1] != '/':
+            path += '/'
+        self.path = os.environ['HOME'] + path
+        self.wto_path = os.environ['WTO']
+        self.sbxml = self.path + 'sbxml/'
+        self.obsxml = self.path + 'obsxml/'
+        self.preferences = pd.Series(
+            ['obsproject.pandas', source, 'sciencegoals.pandas',
+             'scheduling.pandas', 'special.list', 'pwvdata.pandas',
+             'executive.pandas', 'sbxml_table.pandas', 'sbinfo.pandas',
+             'newar.pandas', 'fieldsource.pandas', 'target.pandas',
+             'spectralconf.pandas'],
+            index=['obsproject_table', 'source', 'sciencegoals_table',
+                   'scheduling_table', 'special', 'pwv_data',
+                   'executive_table', 'sbxml_table', 'sbinfo_table',
+                   'newar_table', 'fieldsource_table', 'target_table',
+                   'spectralconf_table'])
+        self.states = ["Approved", "Phase1Submitted", "Broken",
+                       "Canceled", "Rejected"]
+
+        # Global SQL search expressions
+        self.sql1 = str(
+            "SELECT PRJ_ARCHIVE_UID,DELETED,PI,PRJ_NAME,"
+            "CODE,PRJ_TIME_OF_CREATION,PRJ_SCIENTIFIC_RANK,PRJ_VERSION,"
+            "PRJ_LETTER_GRADE,DOMAIN_ENTITY_STATE,"
+            "OBS_PROJECT_ID "
+            "FROM ALMA.BMMV_OBSPROJECT obs1, ALMA.OBS_PROJECT_STATUS obs2 "
+            "WHERE regexp_like (CODE, '^201[23].*\.[AST]') "
+            "AND (PRJ_LETTER_GRADE='A' OR PRJ_LETTER_GRADE='B' "
+            "OR PRJ_LETTER_GRADE='C') "
+            "AND obs2.OBS_PROJECT_ID = obs1.PRJ_ARCHIVE_UID")
+        self.sqlsched_proj = str(
+            "SELECT * FROM SCHEDULING_AOS.OBSPROJECT "
+            "WHERE regexp_like (CODE, '^201[23].*\.[AST]')")
+        self.sqlstates = str(
+            "SELECT DOMAIN_ENTITY_STATE,DOMAIN_ENTITY_ID,OBS_PROJECT_ID "
+            "FROM ALMA.SCHED_BLOCK_STATUS")
+        self.sqlqa0 = str(
+            "SELECT SCHEDBLOCKUID,QA0STATUS FROM ALMA.AQUA_EXECBLOCK "
+            "WHERE regexp_like (OBSPROJECTCODE, '^201[23].*\.[AST]')")
+        self.sqlsched_sb = str(
+            "SELECT ou.OBSUNIT_UID,sb.NAME,sb.REPR_BAND,"
+            "sb.SCHEDBLOCK_CTRL_EXEC_COUNT,sb.SCHEDBLOCK_CTRL_STATE,"
+            "sb.MIN_ANG_RESOLUTION,sb.MAX_ANG_RESOLUTION,"
+            "ou.OBSUNIT_PROJECT_UID "
+            "FROM SCHEDULING_AOS.SCHEDBLOCK sb, SCHEDULING_AOS.OBSUNIT ou "
+            "WHERE sb.SCHEDBLOCKID = ou.OBSUNITID AND sb.CSV = 0")
+
+        # Global Oracle Connection
+        self.connection = cx_Oracle.connect(conx_string)
+        self.cursor = self.connection.cursor()
+
+        # Populate different dataframes related to projects and SBs statuses
+        self.cursor.execute(self.sqlsched_proj)
+        self.scheduling_proj = pd.DataFrame(
+            self.cursor.fetchall(),
+            columns=[rec[0] for rec in self.cursor.description]
+        ).set_index('CODE', drop=False)
+
+        self.cursor.execute(self.sqlstates)
+        self.sbstates = pd.DataFrame(
+            self.cursor.fetchall(),
+            columns=[rec[0] for rec in self.cursor.description]
+        ).set_index('DOMAIN_ENTITY_ID')
+
+        self.cursor.execute(self.sqlqa0)
+        self.qa0 = pd.DataFrame(
+            self.cursor.fetchall(),
+            columns=[rec[0] for rec in self.cursor.description]
+        ).set_index('SCHEDBLOCKUID', drop=False)
+
+        self.cursor.execute(self.sqlsched_sb)
+        self.scheduling_sb = pd.DataFrame(
+            self.cursor.fetchall(),
+            columns=[rec[0] for rec in self.cursor.description]
+        ).set_index('OBSUNIT_UID', drop=False)
+
+        # Initialize with saved data and update, Default behavior.
+        if not self.new:
+            try:
+                self.obsproject = pd.read_pickle(
+                    self.path + self.preferences.obsproject_table)
+                self.sciencegoals = pd.read_pickle(
+                    self.path + self.preferences.sciencegoals_table)
+                self.schedblocks = pd.read_pickle(
+                    self.path + self.preferences.sbxml_table)
+                self.schedblock_info = pd.read_pickle(
+                    self.path + self.preferences.sbinfo_table)
+                self.newar = pd.read_pickle(
+                    self.path + self.preferences.newar_table)
+                self.fieldsource = pd.read_pickle(
+                    self.path + self.preferences.fieldsource_table)
+                self.target = pd.read_pickle(
+                    self.path + self.preferences.target_table)
+                self.spectralconf = pd.read_pickle(
+                    self.path + self.preferences.spectralconf_table)
+            except IOError, e:
+                print e
+                self.new = True
+
+        # Create main dataframes
+        if self.new:
+            call(['rm', '-rf', self.path])
+            print(self.path + ": creating preferences dir")
+            os.mkdir(self.path)
+            os.mkdir(self.sbxml)
+            os.mkdir(self.obsxml)
+            self.start_wto()
+
+    def start_wto(self):
+
+        """
+        Initializes the wtoDatabase dataframes.
+
+        The function queries the archive to look for cycle 1 and cycle 2
+        projects, disregarding any projects with status "Approved",
+        "Phase1Submitted", "Broken", "Canceled" or "Rejected".
+
+        The archive tables used are ALMA.BMMV_OBSPROPOSAL,
+        ALMA.OBS_PROJECT_STATUS, ALMA.BMMV_OBSPROJECT and
+        ALMA.XML_OBSPROJECT_ENTITIES.
+
+        :return: None
+        """
+        # noinspection PyUnusedLocal
+        states = self.states
+
+        sql2 = str(
+            "SELECT PROJECTUID,ASSOCIATEDEXEC "
+            "FROM ALMA.BMMV_OBSPROPOSAL "
+            "WHERE (CYCLE='2012.1' OR CYCLE='2013.1' OR CYCLE='2013.A' "
+            "OR CYCLE='2012.A')")
+
+        self.cursor.execute(sql2)
+        self.executive = pd.DataFrame(
+            self.cursor.fetchall(), columns=['PRJ_ARCHIVE_UID', 'EXEC'])
+
+        if self.source is None:
+            self.cursor.execute(self.sql1)
+            df1 = pd.DataFrame(
+                self.cursor.fetchall(),
+                columns=[rec[0] for rec in self.cursor.description])
+            print(len(df1.query('DOMAIN_ENTITY_STATE not in @states')))
+            self.obsproject = pd.merge(
+                df1.query('DOMAIN_ENTITY_STATE not in @states'), self.executive,
+                on='PRJ_ARCHIVE_UID').set_index('CODE', drop=False)
+        else:
+            if type(self.source) is not str and type(self.source) is not list:
+                print "The source should be a string or a list"
+                return None
+            try:
+                if type(self.source) is str:
+                    fp = open(self.source, 'r')
+                    read_csv = csv.reader(fp)
+                else:
+                    read_csv = self.source
+                c = 0
+                for l in read_csv:
+                    if type(self.source) is str:
+                        l = l[0]
+                    sql3 = self.sql1 + ' AND OBS1.PRJ_CODE = ' + '\'%s\'' % l
+                    self.cursor.execute(sql3)
+                    if c == 0:
+                        df2 = pd.DataFrame(
+                            self.cursor.fetchall(),
+                            columns=[rec[0] for rec in self.cursor.description])
+                    else:
+                        df2.ix[c] = pd.Series(
+                            self.cursor.fetchall()[0], index=df2.columns)
+                    c += 1
+                self.obsproject = pd.merge(
+                    df2.query('DOMAIN_ENTITY_STATE not in @states'),
+                    self.executive,
+                    on='PRJ_ARCHIVE_UID').set_index('CODE', drop=False)
+            except IOError:
+                print "Source filename does not exist"
+                return None
+
+        timestamp = pd.Series(
+            np.zeros(len(self.obsproject), dtype=object),
+            index=self.obsproject.index)
+        self.obsproject['timestamp'] = timestamp
+        self.obsproject['obsproj'] = pd.Series(
+            np.zeros(len(self.obsproject), dtype=object),
+            index=self.obsproject.index)
+        codes = self.obsproject.CODE.tolist()
+        for c in codes:
+            self.get_obsproject(c)
+        self.filter_c1()
+        print len(self.obsproject)
+        self.obsproject.to_pickle(
+            self.path + self.preferences.obsproject_table)
+
+    def get_obsproject(self, code):
+        """
+
+        :param code:
+        """
+        print("Downloading Project %s obsproject.xml" % code)
+        self.cursor.execute(
+            "SELECT TIMESTAMP, XMLTYPE.getClobVal(xml) "
+            "FROM ALMA.XML_OBSPROJECT_ENTITIES "
+            "WHERE ARCHIVE_UID = '%s'" % self.obsproject.ix[
+                code, 'PRJ_ARCHIVE_UID'])
+        data = self.cursor.fetchall()[0]
+        xml_content = data[1].read()
+        xmlfilename = code + '.xml'
+        self.obsproject.loc[code, 'timestamp'] = data[0]
+        filename = self.obsxml + xmlfilename
+        io_file = open(filename, 'w')
+        io_file.write(xml_content)
+        io_file.close()
+        self.obsproject.loc[code, 'obsproj'] = xmlfilename
+
+    def filter_c1(self):
+        """
+
+
+        """
+        c1c2 = pd.read_csv(
+            self.wto_path + 'conf/c1c2.csv', sep=',', header=0,
+            usecols=range(5))
+        c1c2.columns = pd.Index([u'CODE', u'Region', u'ARC', u'C2', u'P2G'],
+                                dtype='object')
+        toc2 = c1c2[c1c2.fillna('no').C2.str.startswith('Yes')]
+        check_c1 = pd.merge(
+            self.obsproject[self.obsproject.CODE.str.startswith('2012')],
+            toc2, on='CODE', how='right').set_index(
+                'CODE', drop=False)[['CODE']]
+        check_c2 = self.obsproject[
+            self.obsproject.CODE.str.startswith('2013')][['CODE']]
+        self.checked = pd.concat([check_c1, check_c2])
+        temp = pd.merge(
+            self.obsproject, self.checked, on='CODE',
+            copy=False).set_index('CODE', drop=False)
+        self.obsproject = temp
+
+
+class ObsProject(object):
+    """
+
+    :param xml_file:
+    :param path:
+    """
+
+    def __init__(self, xml_file, path='./'):
+        """
+
+        :param xml_file:
+        :param path:
+        """
+        io_file = open(path + xml_file)
+        tree = objectify.parse(io_file)
+        io_file.close()
+        data = tree.getroot()
+        self.status = data.attrib['status']
+        for key in data.__dict__:
+            self.__setattr__(key, data.__dict__[key])
+
+    def assoc_sched_blocks(self):
+        """
+
+
+        :return:
+        """
+        result = {}
+
+        try:
+            for sg in self.ObsProgram.ObsPlan.ObsUnitSet:
+                sched_uid = []
+                sgid = sg.attrib['entityPartId']
+                for ous in sg.ObsUnitSet:
+                    try:
+                        for mous in ous.ObsUnitSet:
+                            array_requested = mous.ObsUnitControl.attrib[
+                                'arrayRequested']
+                            try:
+                                for sbs in mous.SchedBlockRef:
+                                    if array_requested in 'TWELVE-M':
+                                        sched_uid.append(
+                                            sbs.attrib['entityId'])
+                                    elif array_requested == 'SEVEN-M':
+                                        sched_uid.append(
+                                            sbs.attrib['entityId'])
+                                    elif array_requested == 'TP-Array':
+                                        sched_uid.append(
+                                            sbs.attrib['entityId'])
+                            except AttributeError:
+                                # Member OUS does not have any SB created yet.
+                                continue
+                    except AttributeError:
+                        print('Project %s has no member OUS in at least one '
+                              'SG_OUS' % self.code)
+                        continue
+                result[sgid] = sched_uid
+        except AttributeError:
+            print "Project %s has no Science Goal OUS" % self.code
+        return result
+
+    def import_sched_blocks(self):
+        pass
+
+
+class SchedBlocK(object):
+
+    def __init__(self, xml_file, path='./'):
+        """
+
+        :param xml_file:
+        :param path:
+        """
+        io_file = open(path + xml_file)
+        tree = objectify.parse(io_file)
+        io_file.close()
+        self.data = tree.getroot()
+
+
+def convert_deg(angle, unit):
+    """
+
+    :param angle:
+    :param unit:
+    :return:
+    """
+    value = angle
+    if unit == 'mas':
+        value /= 3600000.
+    elif unit == 'arcsec':
+        value /= 3600.
+    elif unit == 'arcmin':
+        value /= 60.
+    elif unit == 'rad':
+        value = value * pd.np.pi / 180.
+    elif unit == 'hours':
+        value *= 15.
+    return value
+
+
+def convert_sec(angle, unit):
+    """
+
+    :param angle:
+    :param unit:
+    :return:
+    """
+    value = angle
+    if unit == 'mas':
+        value /= 1000.
+    elif unit == 'arcsec':
+        value /= 1.
+    elif unit == 'arcmin':
+        value *= 60.
+    elif unit == 'rad':
+        value = (value * pd.np.pi / 180.) * 3600.
+    elif unit == 'hours':
+        value *= 15. * 3600.
+    elif unit == 'deg':
+        value *= 3600.
+    else:
+        return None
+    return value
+
+
+def convert_jy(flux, unit):
+    """
+
+    :param flux:
+    :param unit:
+    :return:
+    """
+    value = flux
+    if unit == 'Jy':
+        value = value
+    elif unit == 'mJy':
+        value /= 1000.
+    else:
+        return None
+    return value
+
+
+def convert_mjy(flux, unit):
+    """
+
+    :param flux:
+    :param unit:
+    :return:
+    """
+    value = flux
+    if unit == 'Jy':
+        value *= 1e3
+    elif unit == 'mJy':
+        value = value
+    else:
+        return None
+    return value
+
+
+def convert_ghz(freq, unit):
+    """
+
+    :param freq:
+    :param unit:
+    :return:
+    """
+    value = freq
+    if unit == 'GHz':
+        value = value
+    elif unit == 'MHz':
+        value *= 1e-3
+    elif unit == 'kHz':
+        value *= 1e-6
+    elif unit == 'Hz':
+        value *= 1e-9
+    else:
+        return None
+    return value
+
+
+def convert_tsec(time, unit):
+
+    """
+
+    :param time:
+    :param unit:
+    :return:
+    """
+    value = time
+    if unit == 's':
+        return value
+    elif unit == 'min':
+        return value * 60.
+    elif unit == 'h':
+        return value * 3600.
+    else:
+        return None
