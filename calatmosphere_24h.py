444a03eb535a8a1195bc5e54b3ef3b4c1daef98f
Ignacio Toledo <itoledoc@gmail.com>
2014-11-03 21:35:18 -0300

    adding

--- calatmosphere_24h.py
+++ calatmosphere_24h.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
 __author__ = 'itoledo'
 
 import cx_Oracle
 import pandas as pd
 import optparse
+import ephem
+from astropy.time import Time
 
 
-def query_atm(uid, cursor):
+def query_atm(cursor):
 
+    site = ephem.Observer()
+    day = Time(ephem.date(site.date - 1.).datetime(), format='datetime',
+               scale='utc').mjd * 24 * 3600
+    print('%d' % (day * 1E9))
     sql = str(
         'SELECT ARCHIVE_UID, ANTENNA_NAME, RECEIVER_BAND_ENUMV,'
         'baseband_name_enumv, CAL_DATA_ID,'
         'DBMS_LOB.substr(T_REC_VAL), DBMS_LOB.substr(T_SYS_VAL),'
         'SYSCAL_TYPE_ENUMV, DBMS_LOB.substr(POLARIZATION_TYPES_VAL),'
-        'DBMS_LOB.substr(SB_GAIN_VAL), DBMS_LOB.substr(FREQUENCY_RANGE_VAL) '
+        'DBMS_LOB.substr(SB_GAIN_VAL), DBMS_LOB.substr(FREQUENCY_RANGE_VAL),'
+        'START_VALID_TIME '
         'FROM SCHEDULING_AOS.ASDM_CALATMOSPHERE '
-        'WHERE ARCHIVE_UID = \'%s\'' % uid)
+        'WHERE START_VALID_TIME > %d' % (day * 1E9))
     print(sql)
     print("Executing QUERY, please wait...")
     cursor.execute(sql)
@@ -25,7 +30,7 @@ def query_atm(uid, cursor):
         cursor.fetchall(),
         columns=['UID', 'ANTENNA', 'BAND', 'BB', 'SCAN_ID', 'TREC_VAL',
                  'TSYS_VAL', 'CALTYPE', 'POL_VAL', 'SBGAIN_VAL',
-                 'FREQ_RANGE_VAL'])
+                 'FREQ_RANGE_VAL', 'START_VALID_TIME'])
     return df
 
 
@@ -37,7 +42,6 @@ def extract_atmval(ser):
     for i in range(numpol):
         nombre = pol[i + 3].split('</value>')[0]
         namepol.append('_' + nombre)
-
     rec = ser.TREC_VAL.split(' ')[3:]
     sys = ser.TSYS_VAL.split(' ')[3:]
     gain = ser.SBGAIN_VAL.split(' ')[3:]
@@ -52,12 +56,16 @@ def extract_atmval(ser):
         c += 1
     freqmin = float(ser.FREQ_RANGE_VAL.split(' ')[3]) * 1E-9
     freqmax = float(ser.FREQ_RANGE_VAL.split(' ')[4]) * 1E-9
-    out = [ser.UID, ser.ANTENNA, ser.BAND, ser.BB, ser.SCAN_ID, freqmin,
-           freqmax]
+    date = Time(ser.START_VALID_TIME * 1E-9 / 3600 / 24, format='mjd')
+
+    out = [ser.UID, ser.ANTENNA, ser.BAND, ser.BB,
+           int(ser.SCAN_ID.split('_')[-1]), freqmin, freqmax,
+           date.datetime.isoformat().replace('T', ' ').split('.')[0]]
     out.extend(trec)
     out.extend(tsys)
     out.extend(sbgain)
-    names = ['UID', 'ANTENNA', 'BAND', 'BB', 'SCAN_ID', 'FREQMIN', 'FREQMAX']
+    names = ['UID', 'ANTENNA', 'BAND', 'BB', 'SCAN_ID', 'FREQMIN', 'FREQMAX',
+             'DATE']
     for n in ['trec', 'tsys', 'sbgain']:
         for p in namepol:
             names.append(n + p)
@@ -72,27 +80,14 @@ def main():
     :return:
     """
 
-    usage = "usage: %prog sb_uid\n"
+    usage = "usage: %prog sb_uid"
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option(
-        '-f', '--format', dest='filet', default='xls',
-        help="Select xls or csv as file output")
-
     (options, args) = parser.parse_args()
-
-    if len(args) == 0:
-        print("Please specify sb uid")
-        exit()
-
-    if options.filet not in ['csv', 'xls']:
-        print("-f must be csv or xls")
-        exit()
-
     conx_string = 'almasu/alma4dba@ALMA_ONLINE.OSF.CL'
     connection = cx_Oracle.connect(conx_string)
     cursor = connection.cursor()
 
-    df = query_atm(args[0], cursor)
+    df = query_atm(cursor)
     cursor.close()
     connection.close()
     if len(df) == 0:
@@ -101,15 +96,8 @@ def main():
 
     table = df.apply(lambda r: extract_atmval(r), axis=1)
 
-    if options.filet == 'xls':
-        table.to_excel(
-            '%s_atmosphere.xls' %
-            args[0].replace('uid://', '').replace('/', '_'))
-    else:
-        table.to_csv(
-            '%s_atmosphere.cvs' %
-            args[0].replace('uid://', '').replace('/', '_'),
-            sep=',')
+    table.to_excel('day_atmosphere.xls')
+
     exit()
 
 if __name__ == '__main__':
