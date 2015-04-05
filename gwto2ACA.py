3b545f2995744384ce6f02e6a184a57b231be548
Ignacio Toledo <itoledoc@gmail.com>
2015-04-05 00:16:51 -0300

    fixed small typo

--- gwto2ACA.py
+++ gwto2ACA.py
@@ -532,7 +532,7 @@ class MyStdTableModel(QAbstractTableModel):
             elif col in [9, 19, 20, 22]:
                 return QVariant(QString("%1").arg(sb[col], 0, 'f', 1))
             elif col in [14, 15]:
-                return QVariant(QString("%1").arg(sb[col], 0, 'i', 0))
+                return QVariant(QString("%1").arg(str(sb[col]), 0, 'i', 0))
 
             return QVariant(str(self.arraydata[index.row()][index.column()]))
         elif role == Qt.TextAlignmentRole:
